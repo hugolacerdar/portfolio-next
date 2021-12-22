1f1deec9ad64ec7a344a2019f2050784bcf260ad
Hugo <hugolacerdar@gmail.com>
2021-12-22 13:56:05 -0300

    Add head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import Contact from "../components/Contact";
 import Header from "../components/Header";
 import Presentation from "../components/Presentation";
@@ -8,6 +9,9 @@ const Home: NextPage = () => {
   return (
     <>
       <Header />
+      <Head>
+        <title>Hugo Lacerda{"'"}s Portfolio</title>
+      </Head>
       <Presentation />
       <Projects />
       <Contact />
