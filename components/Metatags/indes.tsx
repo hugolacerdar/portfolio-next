08c8bbcb5ff15192ae86fabe3a025b7355373cc1
Hugo <hugolacerdar@gmail.com>
2021-12-24 16:38:38 -0300

    Add Metatags

--- components/Metatags/indes.tsx
+++ components/Metatags/indes.tsx
@@ -10,10 +10,10 @@ export default function Metatags() {
       <title>{title}</title>
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content="@hugolacerdar" />
-      <meta name="twitter:title" content={`${title} | READNEXT`} />
+      <meta name="twitter:title" content={`${title}`} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={image} />
-      <meta property="og:title" content={`${title} | READNEXT`} />
+      <meta property="og:title" content={`${title}`} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
     </Head>
