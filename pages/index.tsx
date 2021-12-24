78f404dcb9a7874ad867f5cb271d087f92c41162
Hugo <hugolacerdar@gmail.com>
2021-12-24 16:29:41 -0300

    Add Metatags

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,15 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Contact from "../components/Contact";
 import Header from "../components/Header";
+import Metatags from "../components/Metatags/indes";
 import Presentation from "../components/Presentation";
 import Projects from "../components/Projects";
 
 const Home: NextPage = () => {
   return (
     <>
+      <Metatags />
       <Header />
-      <Head>
-        <title>Hugo Lacerda{"'"}s Portfolio</title>
-      </Head>
       <Presentation />
       <Projects />
       <Contact />
